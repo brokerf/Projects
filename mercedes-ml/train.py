7024963a1d821fe888cdaa49ed7e22adef992097
Robert Gorden <robertgorden246@gmail.com>
2025-12-07 12:20:54 +0100

    init new project

---
+++ mercedes-ml/train.py
@@ -0,0 +1,13 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+iris = load_iris()
+model = RandomForestClassifier()
+model.fit(iris.data, iris.target)
+
+joblib.dump(model, "model.pki")
+
+mlflow.sklearn.log_model(model, "model1")
